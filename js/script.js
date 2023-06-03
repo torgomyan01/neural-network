b35ca694d57039167d9c9663e7f066e24fe1fb88
torgomyan1998 <andranik.torgomyan1998@gmail.com>
2023-06-03 14:16:14 +0400

    Add home page

--- js/script.js
+++ js/script.js
@@ -3,52 +3,18 @@ const { none, show, active } = {
     show: 'show',
     active: 'active'
 }
-const Button = $('.btn-sign')
-const menu_bar = document.getElementById("menu-bar" )
-const navbar = document.querySelector('.navbar')
 
-AOS.init();
+const { mobileMenu } = {
+    mobileMenu: $('.mobile-menu')
+}
 
-$('.change-name-user-profile').on('click', function (){
-    $('.user-profile-name').addClass(none);
-    $('.form-change-name-user').removeClass(none)
+$('.mobile-menu-board').on('click', function (){
+    const thisElem = $(this);
+    if(thisElem.hasClass(active)){
+        mobileMenu.removeClass(active)
+        thisElem.removeClass(active)
+    } else {
+        mobileMenu.addClass(active)
+        thisElem.addClass(active)
+    }
 })
-
-$('.save-user-info').on('click', function (){
-    $('.user-profile-name').removeClass(none);
-    $('.form-change-name-user').addClass(none)
-})
-
-$('.close-green-message').on('click', function (){
-    $(this).parent().addClass(none)
-})
-
-
-$('.open-green-message').on('click', function (){
-    $('.green-message').addClass(show);
-
-    setTimeout(() => {
-        $('.green-message').removeClass(show);
-    }, 3000)
-})
-
-Button.on('click', function (){
-    Button.removeClass(active);
-    $(this).addClass(active);
-})
-
-menu_bar.addEventListener('click', ()=>{
-    navbar.classList.toggle('active');
-    menu_bar.classList.toggle('active');
-})
-
-
-$('#table-slider').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: true,
-    appendDots: $('.dots-slider-table'),
-    nextArrow: '.table-next',
-    prevArrow: '.table-prev'
-});
