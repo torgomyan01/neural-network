2be1e155f606a92d353842f63f32f033ff166f38
torgomyan1998 <andranik.torgomyan1998@gmail.com>
2023-06-05 16:06:05 +0400

    Fix bugs, and add modal

--- js/script.js
+++ js/script.js
@@ -62,3 +62,19 @@ defaultInput.on('blur', function (){
 defaultInput.on('focus', function (){
     $(this).parent().addClass(active);
 })
+
+
+const moreModal = new bootstrap.Modal('#more-modal', {
+    keyboard: false
+})
+
+$('.services-item button').on('click', function (){
+    moreModal.show();
+    const title = $(this).parent().prev().children('.services-item-title').text()
+    const text = $(this).data('text');
+
+    $('#more-modal .modal-title').text(title);
+    $('#more-modal .modal-body').text(text);
+
+
+})
