9231864d6b21383ecd5ebfc4f193979879fb8507
torgomyan1998 <andranik.torgomyan1998@gmail.com>
2023-06-16 10:58:35 +0400

    Chan logic site

--- js/script.js
+++ js/script.js
@@ -64,31 +64,3 @@ defaultInput.on('focus', function (){
     $(this).parent().addClass(active);
 })
 
-
-
-const presets = $('.box-col2 .box-content-footer .images .image');
-
-
-presets.on('click', function (){
-    presets.removeClass(active);
-    $(this).addClass(active);
-    $('.def-btn.pink').removeClass(disabled).removeAttr('disabled');
-    $('.close-video').removeClass('active');
-    $('.box-col2 .box-content').css('padding', '0 5rem');
-    $('#upload-block').removeClass(none);
-    $('#video').addClass(none);
-    $('#video source').attr('src', `assets/videos/${this.dataset.videourl}`);
-    $('.box-content-heading').text('File Uploaded')
-
-})
-
-$('.def-btn.pink').on('click', function (){
-    $('.box-col2 .box-content').css('padding', '0');
-    $('#upload-block').addClass(none);
-    $('.close-video').addClass('active');
-    $('#video').removeClass(none);
-    document.getElementById('video').load();
-    document.getElementById('video').play();
-    $('#video source').attr('src', this.dataset.videourl);
-    $('.box-content-heading').text('Upload a video that has people walking. Format: mpg, mpeg, mp4')
-})
